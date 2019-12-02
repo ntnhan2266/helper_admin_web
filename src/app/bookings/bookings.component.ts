bab600fef3a1af251ff97ece11bad92f4c51355c
Nhan Nguyen <ntnhan2266@gmail.com>
2019-12-02 23:23:10 +0700

    Add booking filter

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,36 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { BookingsService } from './bookings.service';
-import { PageEvent } from '@angular/material/paginator';
-import { UtilsService } from 'app/helper/utils.service';
-import { MatDialog, MatDialogConfig, MatDatepickerInputEvent } from '@angular/material';
-import { BookingCancelDialogComponent } from 'app/dialogs/booking-cancel-dialog/booking-cancel-dialog.component';
-import { AppConstants } from 'app/helper/constants';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { BookingsService } from "./bookings.service";
+import { PageEvent } from "@angular/material/paginator";
+import { UtilsService } from "app/helper/utils.service";
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDatepickerInputEvent
+} from "@angular/material";
+import { BookingCancelDialogComponent } from "app/dialogs/booking-cancel-dialog/booking-cancel-dialog.component";
+import { AppConstants } from "app/helper/constants";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+import { FormControl } from "@angular/forms";
+import { UsersService } from "app/users/users.service";
+import { MaidsService } from "app/maids/maids.service";
+
+export interface FormItem {
+  label: string;
+  value: any;
+}
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.scss']
+  selector: "app-bookings",
+  templateUrl: "./bookings.component.html",
+  styleUrls: ["./bookings.component.scss"]
 })
 export class BookingsComponent implements OnInit {
   bookings = [];
   total = 0;
   pageSize = 10;
   pageEvent: PageEvent;
-  query = '';
-  options: string[] = ['One', 'Two', 'Three'];
-  userFilter = '';
-  filterBy = 'helper';
+  query = "";
+  options: FormItem[] = [];
+  userFilter = "";
+  filterBy = "helper";
   date = new Date();
+  queryId = '';
 
   private subject: Subject<string> = new Subject();
 
   constructor(
     private _bookingService: BookingsService,
     private _dialog: MatDialog,
-    private _utilService: UtilsService
+    private _utilService: UtilsService,
+    private _usersService: UsersService,
+    private _maidService: MaidsService,
   ) {}
 
   ngOnInit() {
@@ -42,7 +56,37 @@ export class BookingsComponent implements OnInit {
   }
 
   private changeUserOptions(keyword: string) {
-    console.log(keyword);
+    this.options = [];
+    if (this.filterBy === "user") {
+      this._usersService
+        .list({ pageIndex: 0, pageSize: 5, query: keyword })
+        .subscribe(res => {
+          const users = res.users;
+          for (const user of users) {
+            this.options.push({
+              label: user.name,
+              value: user._id
+            });
+          }
+        });
+    } else {
+      this._maidService
+        .list({pageSize: 5, pageIndex: 0, query: keyword})
+        .subscribe(res => {
+          const maids = res.maids;
+          console.log(maids);
+          for (const maid of maids) {
+            this.options.push({
+              label: maid.user.name,
+              value: maid._id
+            });
+          }
+        })
+    }
+  }
+
+  changeQueryID = (option) => {
+    this.queryId = option.value;
   }
 
   public getData(event?: PageEvent) {
@@ -50,7 +94,9 @@ export class BookingsComponent implements OnInit {
       .list({
         pageIndex: event ? event.pageIndex : 0,
         pageSize: this.pageSize,
-        query: this.query
+        filterBy: this.filterBy,
+        queryId: this.queryId,
+        date: this.date
       })
       .subscribe(result => {
         if (!result.errorCode) {
@@ -72,11 +118,15 @@ export class BookingsComponent implements OnInit {
     this._dialog.open(BookingCancelDialogComponent, dialogConfig);
   }
 
+  getOptionText(option) {
+    return option ? option.label : '';
+  }
+
   public cancelBooking(id, index): void {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = '450px';
+    dialogConfig.width = "450px";
     dialogConfig.data = { id };
     const dialogRef = this._dialog.open(
       BookingCancelDialogComponent,
--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -1,35 +1,36 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-import { APIService } from '../helper/api.service';
-import { CookieService } from 'ngx-cookie-service';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { environment } from "../../environments/environment";
+import { APIService } from "../helper/api.service";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root"
 })
-
 export class BookingsService extends APIService {
-    constructor(public http: HttpClient, 
-        public cookieService: CookieService) {
-        super(http, cookieService);
-    }
+  constructor(public http: HttpClient, public cookieService: CookieService) {
+    super(http, cookieService);
+  }
 
-    list(data): Observable<any> {
-        return this.http.get(environment.apiUrl
-            + `/bookings/list?pageIndex=${data.pageIndex}&pageSize=${data.pageSize}`, this.httpOptions)
-            .pipe(
-                catchError(this.handleError<any>('load_bookings'))
-            );
-    }
+  list(data): Observable<any> {
+    return this.http
+      .get(
+        environment.apiUrl +
+          `/bookings/list?pageIndex=${data.pageIndex}&pageSize=${data.pageSize}&filterBy=${data.filterBy}&queryId=${data.queryId}&date=${data.date}`,
+        this.httpOptions
+      )
+      .pipe(catchError(this.handleError<any>("load_bookings")));
+  }
 
-    cancel(id, content): Observable<any> {
-        return this.http.put(environment.apiUrl
-            + `/booking/admin-cancel`, {id, content}, this.httpOptions)
-            .pipe(
-                catchError(this.handleError<any>('admin_cancel_bookings'))
-            );
-    }
-
+  cancel(id, content): Observable<any> {
+    return this.http
+      .put(
+        environment.apiUrl + `/booking/admin-cancel`,
+        { id, content },
+        this.httpOptions
+      )
+      .pipe(catchError(this.handleError<any>("admin_cancel_bookings")));
+  }
 }
